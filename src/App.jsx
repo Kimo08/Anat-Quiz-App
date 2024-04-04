06d225cc86b3452b82591232caf1b7940e849404
Kimo <kennedy.obasi@prunedge.com>
2024-04-04 21:46:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Anat from "./Component/Anat-Quiz/Anat";
+
+const App = () => {
+  return (
+    <>
+      <Anat />
+    </>
+  );
+};
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,173 @@
+export const data = [
+  {
+    question:
+      "This is defined as a group of cells with a similar structure and function.",
+    option1: "Tissue",
+    option2: "Organ",
+    option3: "Organism",
+    option4: "Molecules",
+    ans: 1,
+  },
+  {
+    question: "In which cavity is the brain located?",
+    option1: "Neural cavity",
+    option2: "Abdominal cavity",
+    option3: "Vertebral cavity",
+    option4: "Cranial cavity",
+    ans: 4,
+  },
+  {
+    question: "A transverse plane will cut a body or organ into:",
+    option1: "Anterior and Posterior",
+    option2: "At an angle",
+    option3: "Left and Right",
+    option4: "Superior and Inferior",
+    ans: 4,
+  },
+  {
+    question: "The sternum is (     ) to the heart.",
+    option1: "Posterior",
+    option2: "Anterior",
+    option3: "Inferior",
+    option4: "Right",
+    ans: 2,
+  },
+  {
+    question: "How many bones are found in the adult human skeleton?",
+    option1: "200",
+    option2: "206",
+    option3: "250",
+    option4: "232",
+    ans: 2,
+  },
+  {
+    question:
+      "If you had to get orthopedic surgery to mend your torn meniscus, what largest joint in the body would you have to get repaired?",
+    option1: "Elbow",
+    option2: "Knee",
+    option3: "Ankle",
+    option4: "Shoulder",
+    ans: 2,
+  },
+  {
+    question:
+      "Of the three principal layers that make up human skin, what name is given to the outer most layer that provides a waterproof and protective barrier?",
+    option1: "Dermis",
+    option2: "Endodermis",
+    option3: "Epidermis",
+    option4: "Hypodermis",
+    ans: 3,
+  },
+  {
+    question:
+      "The islets of Langerhans are groups of cells that produce insulin and glucagon located in what organ of the body?",
+    option1: "Kidney",
+    option2: "Liver",
+    option3: "Pancreas",
+    option4: "Spleen",
+    ans: 3,
+  },
+  {
+    question:
+      "The chloecyst is another name for what small organ that stores bile?",
+    option1: "Spleen",
+    option2: "Gallbladder",
+    option3: "Pancreas",
+    option4: "Appendix",
+    ans: 2,
+  },
+  {
+    question:
+      "Starting at the lower back and running down both legs, which nerve is the longest and widest in the human body?",
+    option1: "Sciatic nerve",
+    option2: "Ulnar nerve",
+    option3: "Saphenous nerve",
+    option4: "Femoral nerve",
+    ans: 1,
+  },
+  {
+    question:
+      "Components of the central nervous system include all of the following except the:",
+    option1: "Spinal nerves",
+    option2: "Diencephalon",
+    option3: "Spinal cord",
+    option4: "Cerebellum",
+    ans: 1,
+  },
+  {
+    question: "The term “rostral” means:",
+    option1: "Towards the dorsal aspect",
+    option2: "Towards the nose",
+    option3: "Towards the ventral aspect",
+    option4: "Towards the tail",
+    ans: 2,
+  },
+  {
+    question: "Which sulcus separates the pons from the midbrain?",
+    option1: "Inferior Pontine Sulcus",
+    option2: "Ventral Sulcus",
+    option3: "Lateral Sulcus",
+    option4: "Superior Pontine Sulcus",
+    ans: 4,
+  },
+  {
+    question: "The trigeminal nerve arises from the brain at the level of the:",
+    option1: "Medulla",
+    option2: "Midbrian",
+    option3: "Mid-Pons",
+    option4: "Diencephalon",
+    ans: 3,
+  },
+  {
+    question:
+      "In early embryogenesis, the notochord develops in the midline from the:",
+    option1: "Floor plate",
+    option2: "Mesoderm",
+    option3: "Ectoderm",
+    option4: "Neural Plate",
+    ans: 2,
+  },
+  {
+    question:
+      "The secondary embryonic brain vesicle that gives rise to the pons and cerebellum is the:",
+    option1: "Mesencephalon",
+    option2: "Telencephalon",
+    option3: "Myelencephalon",
+    option4: "Metencephalon",
+    ans: 4,
+  },
+  {
+    question: "The following is NOT true about retinal cones:",
+    option1: "They produce sharp images, when excited",
+    option2: "They are used in bright light",
+    option3: "They need retinol to work ",
+    option4: "They are less numerous than the rods",
+    ans: 3,
+  },
+  {
+    question:
+      "The ossicle that gets moved by the vibrations of the tympanic membrane is: ",
+    option1: "Malleus",
+    option2: "Incus",
+    option3: "Stapes",
+    option4: "Anvil",
+    ans: 1,
+  },
+  {
+    question: "The function of arrector pili muscle is:",
+    option1: "To contract to squeeze oil from the sebaceous glands",
+    option2: "To squeeze the sweat out of glands",
+    option3: "To contract in order to produce heat for heat regulation",
+    option4: "To raise the hairs in upright position",
+    ans: 4,
+  },
+  {
+    question:
+      "he following brain waves are produced during the REM period of sleep cycle:",
+    option1: "Beta waves",
+    option2: "Alpha waves",
+    option3: "Keppa waves",
+    option4: "Delta waves",
+    ans: 1,
+  },
+];
